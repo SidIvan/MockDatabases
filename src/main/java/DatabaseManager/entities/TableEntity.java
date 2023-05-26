49000b695ed6fb267fdc91cb0639892e4726fb94
SidIvan <sidorenkoivanalexeevich@gmail.com>
2023-05-26 14:31:24 +0300

    Refactoring

--- src/main/java/DatabaseManager/entities/TableEntity.java
+++ src/main/java/DatabaseManager/entities/TableEntity.java
@@ -2,6 +2,7 @@ package DatabaseManager.entities;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import java.util.Set;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "table_entities")
 public class TableEntity {
